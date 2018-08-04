82d51a0015f0036cb480586ec080c9506ee93bc3
xyh <xyh@126.com>
2018-08-04 15:01:21 +0800

    1st

---
+++ src/main/java/bean/Main.java
@@ -0,0 +1,16 @@
+package bean;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DiConfig.class);
+		UseFunctionService useFunctionService = 
+				context.getBean(UseFunctionService.class);
+		System.out.println(useFunctionService.sayHello("world"));
+		context.close();
+	}
+
+}
